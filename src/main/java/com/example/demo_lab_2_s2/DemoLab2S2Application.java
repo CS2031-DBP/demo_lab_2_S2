a69298cbc19cb22e518e39590c4393c10c8dfa7d
mateonoel2 <mateo.noel@utec.edu.pe>
2024-08-26 10:35:24 -0500

    clase a

---
+++ src/main/java/com/example/demo_lab_2_s2/DemoLab2S2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo_lab_2_s2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLab2S2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoLab2S2Application.class, args);
+    }
+
+}
